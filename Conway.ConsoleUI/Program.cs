7b490cce15378fda0d73dc03225d6c21970ba259
p00293412 <jakob.hendrix@la.gov>
2019-09-06 14:12:27 -0500

    Got the blinker working on a 5x5 grid

---
+++ Conway.ConsoleUI/Program.cs
@@ -0,0 +1,50 @@
+﻿using Conway.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conway.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = new LifeGrid();
+
+            // Seed our organism
+            grid.CurrentState[1, 2] = CellState.Alive;
+            grid.CurrentState[2, 2] = CellState.Alive;
+            grid.CurrentState[3, 2] = CellState.Alive;
+
+            ShowGrid(grid.CurrentState);
+
+            while(Console.ReadLine().ToLower() != "q")
+            {
+                grid.UpdateState();
+                ShowGrid(grid.CurrentState);
+            }
+        }
+
+        private static void ShowGrid(CellState[,] currentState)
+        {
+            Console.Clear();
+            int x = 0;
+            int rowLength = 5;
+
+            // Draw our grid, row by row
+            foreach (var state in currentState)
+            {
+                var output = state == CellState.Alive ? "O" : ".";
+                Console.Write(output);
+                x++;
+                if (x >= rowLength)
+                {
+                    x = 0;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
